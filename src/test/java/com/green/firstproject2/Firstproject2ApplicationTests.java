2486fabe3b2ec94e13bfd935b76fb8d904808cbc
sbsteacher <id@gmail.com>
2025-07-15 15:32:20 +0900

    first

---
+++ src/test/java/com/green/firstproject2/Firstproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.firstproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firstproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
